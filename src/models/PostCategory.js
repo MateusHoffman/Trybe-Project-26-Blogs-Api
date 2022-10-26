a8945bfe148df2c3d77ab1e826162650257075a1
MateusHoffman <mateushoffmandev@gmail.com>
2022-10-26 13:50:46 -0300

    11 done

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,6 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
 	const PostCategory = sequelize.define('PostCategory',
-		{},
+		{
+			postId: { type: DataTypes.INTEGER, foreignKey:true, primaryKey: true },
+			categoryId: { type: DataTypes.INTEGER, foreignKey:true, primaryKey: true },
+		},
 		{
 			tableName: 'posts_categories',
 			timestamps: false,
